a0ab5848071cd44ca9498f923dc370dad6738e25
ccp <caochaoping@wr.cn>
2016-03-29 08:28:25 +0000

    Add Auth

--- server/app/app.rb
+++ server/app/app.rb
@@ -6,6 +6,9 @@ Dir["#{ALIEZ_ROOT}/app/controllers/*rb"].each { |f| require f }
 class App
   def initialize
     @app = Rack::Builder.app do
+      use Rack::JWT::Auth, secret: Settings[:session][:secret], exclude: ['/authenticate']
+      map('/authenticate') { run(App.authenticate_controller.new) }
+
       controllers = ObjectSpace.each_object(Class).select { |klass| klass.include?(Endpoint) }
       controllers.each do |controller|
         map(controller.prefix_uri) { run(controller.new) }
@@ -15,5 +18,14 @@ class App
 
   def call(env)
     @app.call(env)
+  end
+
+  private
+
+  def self.authenticate_controller
+    Class.new(Sinatra::Base) do
+      post '/' do
+      end
+    end
   end
 end
