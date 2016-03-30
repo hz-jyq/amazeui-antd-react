1ad61bd181e33cc3805f54d0c33061e2500733fa
ccp <caochaoping@wr.cn>
2016-03-30 03:53:06 +0000

    Impl user authenticate

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < Sinatra::Base
   include Endpoint
 
   post '/authenticate' do
-    user = User.find_by(name: params[:user][:name])
-    if user.authenticate(params[:user][:password])
-      Rack::JWT::Token.encode({ sub: user.id }, Settings[:session][:secret])
-    end
+    user = User.where(name: params[:user][:name]).first
+    halt 401, jbuilder(%(json.error 'wrong username or password')) if user.nil?
+    halt 401, jbuilder(%(json.error 'wrong username or password')) unless user.authenticate(params[:user][:password])
+
+    token = Rack::JWT::Token.encode({ sub: user.id }, Settings[:session][:secret])
+    jbuilder %(json.token '#{token}')
   end
 end
