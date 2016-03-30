eba3937e2879c889a186b73b32bc42bf23bbc62a
ccp <caochaoping@wr.cn>
2016-03-30 06:43:57 +0000

    Add model suggestion

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -5,6 +5,8 @@ class User
   field :name, type: String
   field :password_digest, type: String
 
+  has_many :suggestions
+
   validates :name, presence: true, uniqueness: true
 
   has_secure_password
