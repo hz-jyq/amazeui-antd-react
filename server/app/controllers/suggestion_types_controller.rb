c4821389d422fa82b1f7ec301232ef92ad5900d7
ccp <caochaoping@wr.cn>
2016-04-01 01:31:11 +0800

    Add route: POST /suggestion_types

--- server/app/controllers/suggestion_types_controller.rb
+++ server/app/controllers/suggestion_types_controller.rb
@@ -1,3 +1,22 @@
 class SuggestionTypesController < Sinatra::Base
   include Endpoint
+
+  template :suggestion_type do
+    <<-EOT
+      json.(s, :id, :name, :description)
+      json.public s.public?
+      json.reviewers s.reviewers, :id, :name
+    EOT
+  end
+
+  post '/' do
+    halt 401, jbuilder(%(json.error 'forbidden')) unless [:manager, :administrator].include?(current_user.role)
+
+    p = params[:suggestion_type]
+    s = SuggestionType.new(p.slice(:name, :description, :public))
+    s.reviewers = User.find(p[:reviewers].map(&->(r) { r[:id] }))
+    s.save!
+
+    halt 201, jbuilder(:suggestion_type, locals: { s: s })
+  end
 end
