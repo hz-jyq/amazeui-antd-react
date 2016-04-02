6a6f9c06e2b6a507d25982d9b252895317203cbc
ccp <caochaoping@wr.cn>
2016-04-02 09:42:16 +0800

    Add rspec code: model user

---
+++ server/spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe User, type: :model do
+  it { is_expected.to be_timestamped_document }
+  it { is_expected.to have_field(:name) }
+  it { is_expected.to have_field(:role) }
+
+  it { is_expected.to have_many(:suggestions) }
+  it { is_expected.to have_and_belong_to_many(:suggestion_types) }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
+  it { is_expected.to validate_inclusion_of(:role).to_allow(:user, :manager, :administrator) }
+end
