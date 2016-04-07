9de8370726c31d06dc8c5771316df66d6b5812c0
ccp <caochaoping@wr.cn>
2016-04-07 02:28:46 +0000

    优化测试用例

--- server/spec/controllers/suggestion_types_controller_spec.rb
+++ server/spec/controllers/suggestion_types_controller_spec.rb
@@ -1,12 +1,58 @@
 RSpec.describe SuggestionTypesController, type: :controller do
-  context 'a normal user' do
+  let(:user) { create(:user, role: :manager) }
+  before(:each) { authorize(user) }
+
+  describe 'GET /suggestion_types' do
+    it 'expected response code 200' do
+      create_list(:suggestion_type, 4)
+      get '/suggestion_types'
+      expect(response_status).to eq(200)
+      expect(response_body_as_json.size).to eq(4)
+    end
+  end
+
+  describe 'POST /suggestion_types' do
+    it 'expected response code 201' do
+      post '/suggestion_types', suggestion_type: { name: '创新建议', reviewer_ids: [user.id] }
+      expect(response_status).to eq(201)
+
+      keys = %w(id name description public reviewers)
+      keys.each { |key| expect(response_body_as_json).to have_key(key) }
+      expect(response_body_as_json['name']).to eq('创新建议')
+      expect(response_body_as_json['reviewers'][0]['name']).to eq(user.name)
+    end
+  end
+
+  describe 'GET /suggestion_types/:id' do
+    it 'expected response code 200' do
+      s = create(:suggestion_type, name: '创新建议')
+      get "/suggestion_types/#{s.id}"
+      expect(response_status).to eq(200)
+    end
+  end
+
+  describe 'PUT /suggestion_types/:id' do
+    it 'expected response code 200' do
+      s = create(:suggestion_type, name: '创新建议')
+      put "/suggestion_types/#{s.id}", suggestion_type: { name: '流程管理' }
+      expect(response_status).to eq(200)
+      expect(response_body_as_json['name']).to eq('流程管理')
+    end
+  end
+
+  describe 'DELETE /suggestion_types/:id' do
+    it 'expected response code 204' do
+      s = create(:suggestion_type)
+      delete "/suggestion_types/#{s.id}"
+      expect(response_status).to eq(204)
+    end
+  end
+
+  describe 'Access Control' do
     let(:user) { create(:user) }
     before(:each) { authorize(user) }
 
-    it 'could only access :index' do
-      get '/suggestion_types'
-      expect(response_status).to eq(200)
-
+    it 'expected response code 403 when not granted' do
       post '/suggestion_types'
       expect(response_status).to eq(403)
 
@@ -18,57 +64,6 @@ RSpec.describe SuggestionTypesController, type: :controller do
 
       delete '/suggestion_types/1'
       expect(response_status).to eq(403)
-    end
-  end
-
-  context 'a manager' do
-    let(:user) { create(:user, role: :manager) }
-    before(:each) { authorize(user) }
-
-    describe 'GET /suggestion_types' do
-      it 'expected response code 200' do
-        create_list(:suggestion_type, 4)
-        get '/suggestion_types'
-        expect(response_status).to eq(200)
-        expect(response_body_as_json.size).to eq(4)
-      end
-    end
-
-    describe 'POST /suggestion_types' do
-      it 'expected response code 201' do
-        post '/suggestion_types', suggestion_type: { name: '创新建议', reviewer_ids: [user.id] }
-        expect(response_status).to eq(201)
-
-        keys = %w(id name description public reviewers)
-        keys.each { |key| expect(response_body_as_json).to have_key(key) }
-        expect(response_body_as_json['name']).to eq('创新建议')
-        expect(response_body_as_json['reviewers'][0]['name']).to eq(user.name)
-      end
-    end
-
-    describe 'GET /suggestion_types/:id' do
-      it 'expected response code 200' do
-        s = create(:suggestion_type, name: '创新建议')
-        get "/suggestion_types/#{s.id}"
-        expect(response_status).to eq(200)
-      end
-    end
-
-    describe 'PUT /suggestion_types/:id' do
-      it 'expected response code 200' do
-        s = create(:suggestion_type, name: '创新建议')
-        put "/suggestion_types/#{s.id}", suggestion_type: { name: '流程管理' }
-        expect(response_status).to eq(200)
-        expect(response_body_as_json['name']).to eq('流程管理')
-      end
-    end
-
-    describe 'DELETE /suggestion_types/:id' do
-      it 'expected response code 204' do
-        s = create(:suggestion_type)
-        delete "/suggestion_types/#{s.id}"
-        expect(response_status).to eq(204)
-      end
     end
   end
 end
