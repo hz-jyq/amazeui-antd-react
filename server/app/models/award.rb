7544dac8b42ffd1937fba5d69814d15891aacf2f
ccp <caochaoping@wr.cn>
2016-04-11 00:03:18 +0800

    加入 Award 校验规则

--- server/app/models/award.rb
+++ server/app/models/award.rb
@@ -2,9 +2,16 @@ class Award
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  field :action, type: String
   field :awarded, type: Boolean, default: false
 
   belongs_to :holder, class_name: 'User', inverse_of: :awards
   belongs_to :presenter, class_name: 'User', inverse_of: :award_awards
   belongs_to :suggestion, inverse_of: :awards
+
+  validates :action, presence: true
+  validates :awarded, inclusion: [true, false]
+  validates :holder, presence: true
+  validates :presenter, presence: true
+  validates :suggestion, presence: true
 end
