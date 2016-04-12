eac6a0d91d6912b43993f26bdd6ec6d628160092
ccp <caochaoping@wr.cn>
2016-04-12 03:39:43 +0000

    处理所有未支持的路由，统一返回 404

--- server/app/app.rb
+++ server/app/app.rb
@@ -6,17 +6,21 @@ Dir["#{ALIEZ_ROOT}/app/controllers/concerns/*rb"].each { |f| require f }
 Dir["#{ALIEZ_ROOT}/app/controllers/*rb"].each { |f| require f }
 
 class App
+  class UnknownRouteHandler
+    def call(_env)
+      [404, { 'Content-Type' => 'application/json' }, ['Not Found'.freeze]]
+    end
+  end
+
   def initialize
-    @app = Rack::Builder.app do
+    @app = Rack::Builder.app(UnknownRouteHandler.new) do
       use Rack::PostBodyContentTypeParser
       use Rack::JWT::Auth, secret: Settings[:session][:secret], exclude: ['/users/authenticate']
 
       controllers = Object.constants.grep(/Controller\z/).map { |c| Object.const_get(c) }
       controllers.each do |controller|
-        map(controller.route_url) { run(controller.new) }
+        map(controller.route_url) { run(controller.new(UnknownRouteHandler.new)) }
       end
-
-      run ->(_env) { [404, { 'Content-Type' => 'application/json' }, ['Not Found'.freeze]] }
     end
   end
 
