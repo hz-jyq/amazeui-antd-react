be5abd47047ee4ec67894b70faf63f5c1f855fe8
ccp <caochaoping@wr.cn>
2016-05-05 01:55:55 +0000

    移除不必要的依赖

--- client.next/app/index.jsx
+++ client.next/app/index.jsx
@@ -6,7 +6,7 @@ import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import createLogger from 'redux-logger'
 
-import * as reducers from './ducks'
+import * as reducers from './reducers'
 import { AppContainer } from './containers'
 
 
