e13b325ab126efca1a1a91a0bb4e53f14b640e83
ccp <caochaoping@wr.cn>
2016-05-06 09:46:44 +0000

    新增登录对话框

--- client.next/app/containers/AppContainer.jsx
+++ client.next/app/containers/AppContainer.jsx
@@ -1,17 +1,51 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
+import { Jumbotron, Col, Panel } from 'react-bootstrap'
 
-import NavBar from '../components/NavBar'
+import GlobalNavBar from '../components/GlobalNavBar'
+import SignInForm from '../components/SignInForm'
 
 
 class App extends Component {
   componentWillMount() {
-    // TODO
+    if (this.props.accessToken !== '') {
+      // TODO
+    }
   }
 
   render() {
+    let contentDiv = ''
+    if (this.props.isAuthenticated) {
+      contentDiv = (
+        <Jumbotron>
+          <h1>您好，有什么需要帮助的吗？</h1>
+        </Jumbotron>
+      )
+    } else {
+      if (this.props.accessToken === '') {
+        contentDiv = (
+          <Col sm={4} smOffset={4}>
+            <Panel>
+              <SignInForm />
+            </Panel>
+          </Col>
+        )
+      } else {
+        // TODO
+      }
+    }
+
     return (
-      <NavBar isAuthenticated={this.props.isAuthenticated} userName={this.props.userName} />
+      <div>
+        <GlobalNavBar
+          isAuthenticated={this.props.isAuthenticated}
+          userName={this.props.userName}
+        />
+
+        <div className="container">
+          {contentDiv}
+        </div>
+      </div>
     )
   }
 }
